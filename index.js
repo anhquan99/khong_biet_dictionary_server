29bf51cffb280e49a78ed3c1155817fe4cdb7c90
anhquan99 <anhquanala@gmail.com>
2022-09-10 13:46:43 +0700

    update environment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { ApolloServer, gql } = require("apollo-server");
 const mongoose = require("mongoose");
 const { MONGODB } = require("./config");
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.

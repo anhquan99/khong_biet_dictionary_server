5da532492e307609f153a1cf2f955ff560a257a7
anhquan99 <anhquanala@gmail.com>
2022-09-10 17:33:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,6 +9,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "dev",
   HOST: process.env.HOST || "localhost",
   PORT: process.env.PORT || 4000,
-  MONGODB: process.env.CONNECTION_STR || "mongodb://localhost:27017",
+  MONGODB: process.env.MONGODB || "mongodb://localhost:27017",
   SECRET_KEY: process.env.SECRET_KEY || "private key"
 };

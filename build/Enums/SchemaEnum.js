23d30f23de47ba40b14fd4857f9148c6a8eb8619
anhquan99 <anhquanala@gmail.com>
2023-03-26 20:03:31 +0700

    update gitignore

--- build/Enums/SchemaEnum.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports._maxStringLength = exports.roleEnum = void 0;
-exports.roleEnum = ["admin", "user"];
-exports._maxStringLength = 500;
--- build/Validations/BaseValidation.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IsNullOrEmptyString = void 0;
-function IsNullOrEmptyString(str) {
-    return str === null || str.trim() === '';
-}
-exports.IsNullOrEmptyString = IsNullOrEmptyString;

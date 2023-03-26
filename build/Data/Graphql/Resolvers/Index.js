bed3c529a8569a5368c058df3fe6a7dbbe4cbe69
anhquan99 <anhquanala@gmail.com>
2023-03-26 12:01:49 +0700

    remove build folder

--- build/Data/Graphql/Resolvers/Index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Words_1 = require("./Words");
-const resolvers = {
-    Query: Object.assign({}, Words_1.Words.Query),
-    Mutation: Object.assign({}, Words_1.Words.Mutation)
-};
-exports.default = resolvers;
